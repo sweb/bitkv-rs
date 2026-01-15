63b08d0c1b4e9e0725f14e4ea0953e62ec7548d0
Florian Müller <florian@tomueller.de>
2026-01-15 21:04:53 +0100

    removing some clones

--- src/main.rs
+++ src/main.rs
@@ -8,10 +8,10 @@ fn main() {
     store.set("user1".to_string(), "Alice".to_string()).unwrap();
     store.set("user2".to_string(), "Bob".to_string()).unwrap();
 
-    let val = store.get("user1".to_string()).unwrap();
+    let val = store.get("user1").unwrap();
     println!("Got value: {:?}", val); // Should be Some("Alice")
     store.remove("user1".to_string()).unwrap();
-    let val = store.get("user1".to_string()).unwrap();
+    let val = store.get("user1").unwrap();
     println!("Got value: {:?}", val); // Should be Some("Alice")
     store.compact().unwrap();
 
